1f2dea3a90827d879690cf6bf7df55897f6b753f
kimgwisoo <kimgs243@gmail.com>
2020-05-19 19:38:41 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wid Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
-          sub="HTML is HyperText Markup Language."
+          desc="HTML is HyperText Markup Language."
         ></Content>
       </div>
     );
